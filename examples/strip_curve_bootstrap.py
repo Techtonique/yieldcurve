d57e0746115e4f9b6e98457eb4f1941e7ba8302f
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-01-17 02:39:44 +0100

    fix bootstrapping Pt.2

--- examples/strip_curve_bootstrap.py
+++ examples/strip_curve_bootstrap.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from yieldcurveml.utils import get_swap_rates, regression_report
 from yieldcurveml.stripcurve import CurveStripper
 
+
 datasets = ["and07", "ab13ois", "ap10", "ab13e6m", "negativerates"]
 
 def main():
@@ -18,10 +19,8 @@ def main():
         
         # Plot the results
         plt.figure(figsize=(10, 6))
-        plt.plot(stripper_bootstrap.rates_.maturities, stripper_bootstrap.rates_.swap_rates * 100, 
-                label='Zero Rates', linewidth=2)
-        plt.plot(data.maturity, data.rate * 100, 'o', 
-                label='Market Swap Rates', markersize=8)
+        plt.plot(data.maturity, data.rate * 100, 
+                label='Market Swap Rates')
         plt.title(f'Bootstrapped Zero Curve - {dataset}')
         plt.xlabel('Time to Maturity (years)')
         plt.ylabel('Rate (%)')
